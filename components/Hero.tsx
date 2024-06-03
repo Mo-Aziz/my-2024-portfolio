c5d9d1f28122564d6c841387e9a7f1f5b83300c5
Mo-Aziz <mohamed.aref32@gmail.com>
2024-06-03 17:10:55 -0500

    finished the hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,36 @@
-import React from "react";
-import { Spotlight } from "./ui/Spotlight";
+import React from 'react';
+import { Spotlight } from './ui/Spotlight';
+import { TextGenerateEffect } from './ui/TextGenerateEffect';
+import { FaLocationArrow } from 'react-icons/fa';
+
+import MagicButton from './ui/MagicButton';
 
 function Hero() {
   return (
     <div className="pb-20 pt-36">
       <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
-        />
-        <Spotlight
-          className="top-10 left-full h-[80vh] w-[50vw]"
-          fill="purple"
-        />
-        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+        <Spotlight className="-left-10 -top-40 h-screen md:-left-32 md:-top-20" fill="white" />
+        <Spotlight className="left-full top-10 h-[80vh] w-[50vw]" fill="purple" />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <div className="absolute left-0 top-0 flex h-screen w-full items-center justify-center bg-white bg-grid-black/[0.2] dark:bg-black-100 dark:bg-grid-white/[0.03]">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
+      </div>
+      <div className="relative z-10 my-20 flex justify-center">
+        <div className="flex max-w-[89vw] flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]">
+          <h2 className="max-w-80 text-center text-xs uppercase tracking-widest text-blue-100">
+            Dynamic Web Magic with Next.js
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless User Experiences"
+          />
+          <p>Hi, I&apos;m Maximilian, a Next.js Frontend Engineer based in Austin Texas.</p>
+          <a href="/about">
+            <MagicButton title="Show my work" icon={<FaLocationArrow />} position="right" />
+          </a>
+        </div>
       </div>
     </div>
   );
